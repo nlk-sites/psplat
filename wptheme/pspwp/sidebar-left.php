790b4c5bf1390e93806e96912049b79dcd84461e
Tim Spinks <tim@ninthlink.com>
2014-01-03 18:35:03 +0000

    changed brett combe to ps platinum

--- wptheme/pspwp/sidebar-left.php
+++ wptheme/pspwp/sidebar-left.php
@@ -19,7 +19,7 @@
     </div>
     <hr class="lightbar_hr"><a href="mailto: info@psplatinum.com"><img src="<?php bloginfo('template_url'); ?>/images/featured_listings_email_us.png" width="242" height="29"></a><hr class="lightbar_hr"> <!-- <img src="<?php bloginfo('template_url'); ?>/images/featured_listings_showing.png" width="242" height="33"><hr class="lightbar_hr"><img src="<?php bloginfo('template_url'); ?>/images/featured_listings_print.png" width="243" height="33"><hr class="lightbar_hr"> -->
     <div id="featured_follow">
-        <a href="https://www.facebook.com/pages/PS-Platinum-Properties/202142967222?ref=hl"><img src="<?php bloginfo('template_url'); ?>/images/featured_listings_fb_icon.png" width="32" height="32"></a><a href="https://twitter.com/psplatinum"><img src="<?php bloginfo('template_url'); ?>/images/featured_listings_twitter_icon.png" width="32" height="32"></a><a href="http://www.linkedin.com/in/brettcombs"><img src="<?php bloginfo('template_url'); ?>/images/featured_listings_in_icon.png" width="32" height="32"></a><span class="featured_follow_label">FOLLOW // BRETT COMBS</span>
+        <a href="https://www.facebook.com/pages/PS-Platinum-Properties/202142967222?ref=hl"><img src="<?php bloginfo('template_url'); ?>/images/featured_listings_fb_icon.png" width="32" height="32"></a><a href="https://twitter.com/psplatinum"><img src="<?php bloginfo('template_url'); ?>/images/featured_listings_twitter_icon.png" width="32" height="32"></a><a href="http://www.linkedin.com/in/brettcombs"><img src="<?php bloginfo('template_url'); ?>/images/featured_listings_in_icon.png" width="32" height="32"></a><span class="featured_follow_label">FOLLOW // PS PLATINUM</span>
     </div>
     <hr class="lightbar_hr">
     <br>
