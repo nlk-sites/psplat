3508f76ed5a794c176361c936aa1541d222dd9c2
Tim Spinks <tim@ninthlink.com>
2014-01-28 18:54:48 +0000

    featured listings, links updated, images updates

--- wptheme/pspwp/template-propertylistings.php
+++ wptheme/pspwp/template-propertylistings.php
@@ -79,19 +79,26 @@ get_header(); ?>
 						<?php $count += 1;?>
 						<div class="clearfix"></div>
 		                <div class="featured_listing">
-		                	<img class="featured_thumb" src="<?php bloginfo('template_url'); ?>/images/featured_sample_image.jpg" width="170" height="110">
+		                	<?php 
+								if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+								  the_post_thumbnail( array(170,110), array('class'	=> "featured_thumb") );
+								}
+								else { ?>
+									<img class="featured_thumb" src="<?php bloginfo('template_url'); ?>/images/featured_sample_image.jpg" width="170" height="110" />
+								<?php }
+							?>
+		                	<!--img class="featured_thumb" src="<?php bloginfo('template_url'); ?>/images/featured_sample_image.jpg" width="170" height="110"-->
 		      				<div class="featured_title"><?php the_title(); ?></div>
 		     	 			<div class="featured_price"><img src="<?php bloginfo('template_url'); ?>/images/featured_listings_up_arrow.png" width="15" height="23">&nbsp;<span class="featured_price_txt">$<?php echo number_format(floatval(get_post_meta(get_the_ID(), 'wpcf-current-price', true))); ?></span></div>
 		      				<span class="featured_price_subtxt">Original Price: $<?php echo number_format(floatval(get_post_meta(get_the_ID(), 'wpcf-orginal-price', true))); ?></span>
 		                    <span class="featured_label">bed/bath</span>&nbsp;<span class="featured_txt"><?php echo types_render_field("bed", array()); ?> bedroom | <?php echo types_render_field("bath", array()); ?> bathroom</span><br>
 		      				<span class="featured_label">sq/ft</span>&nbsp;<span class="featured_txt"><?php echo types_render_field("square-feet", array()); ?></span><br>
-		                    
-		    				
 		           		</div>
 		                <div class="featured_links">
 		                  <a target="_blank" href="http://maps.google.com/?q=<?php echo urlencode(get_post_meta(get_the_ID(), 'wpcf-address', true)); ?>"><img src="<?php bloginfo('template_url'); ?>/images/featured_listings_view_map.png" width="115" height="23"></a>
-		                  <a class="showvideopopup" href="#inline<?php echo $count; ?>"><img src="<?php bloginfo('template_url'); ?>/images/featured_listings_watch_video.png" width="115" height="30"></a>
-						  <div id="inline<?php echo $count; ?>" style="text-align: center; display:none;"><?php echo(types_render_field( "video-code", array())); ?><h3><i><?php the_title(); ?></i></h3></div>
+		                  <a class="showvideopopup" href="<?php the_permalink(); ?>#tabs-1"><img src="<?php bloginfo('template_url'); ?>/images/featured_listings_watch_video.png" width="115" height="30"></a>
+						  <?php /* <div id="inline<?php echo $count; ?>" style="text-align: center; display:none;"><?php echo(types_render_field( "video-code", array())); ?><h3><i><?php the_title(); ?></i></h3></div> */ ?>
+		                  <?php /*
 		                  <div class="galleryContainer" style="display: none;">
 						  	<?php
 								$galleryimages = get_post_meta(get_the_ID(), 'wpcf-gallery-image');
@@ -110,9 +117,10 @@ get_header(); ?>
 								
 							?>
 						  </div>
-						  <?php if($firstimg != '#'): ?>
-						  <a href="<?php echo $firstimg; ?>" class="fancybox" rel="gallery-<?php echo get_the_ID(); ?>"><img src="<?php bloginfo('template_url'); ?>/images/featured_listings_photo_gallery.png" width="115" height="30"></a>
-						  <?php endif;?>
+						  */ ?>
+						  <?php //if($firstimg != '#'): ?>
+						  <a href="<?php the_permalink(); ?>#tabs-1" class="" rel="gallery-<?php echo get_the_ID(); ?>"><img src="<?php bloginfo('template_url'); ?>/images/featured_listings_photo_gallery.png" width="115" height="30"></a>
+						  <?php //endif;?>
 		                  <a href="<?php the_permalink(); ?>"><img class="property_details_btn" src="<?php bloginfo('template_url'); ?>/images/featured_listings_prop_details_bttn.png" width="114" height="22"></a>
 		                </div>
 		        		<div class="clearfix"></div>
