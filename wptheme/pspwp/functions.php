fa8f960f11ba7b8236190fcc02441f07827f7199
Tim Spinks <tim@ninthlink.com>
2014-04-09 23:44:53 +0000

    updated funcs

--- wptheme/pspwp/functions.php
+++ wptheme/pspwp/functions.php
@@ -435,3 +435,56 @@ function shortcode_get_site_dir( $atts ) {
 
 }
 add_shortcode( 'get_blog_info', 'shortcode_get_site_dir' );
+
+
+if( !function_exists( 'get_the_post_thumbnail_by_blog' ) ) {
+	function get_the_post_thumbnail_by_blog($blog_id=NULL,$post_id=NULL,$size='post-thumbnail',$attrs=NULL) {
+		global $current_blog;
+		$sameblog = false;
+
+		if( empty( $blog_id ) || $blog_id == $current_blog->ID ) {
+			$blog_id = $current_blog->ID;
+			$sameblog = true;
+		}
+		if( empty( $post_id ) ) {
+			global $post;
+			$post_id = $post->ID;
+		}
+		if( $sameblog )
+			return get_the_post_thumbnail( $post_id, $size, $attrs );
+
+		if( !has_post_thumbnail_by_blog($blog_id,$post_id) )
+			return false;
+
+		global $wpdb;
+		$oldblog = $wpdb->set_blog_id( $blog_id );
+
+		$blogdetails = get_blog_details( $blog_id );
+		$thumbcode = str_replace( $current_blog->domain . $current_blog->path, $blogdetails->domain . $blogdetails->path, get_the_post_thumbnail( $post_id, $size, $attrs ) );
+
+		$wpdb->set_blog_id( $oldblog );
+		return $thumbcode;
+	}
+
+	function has_post_thumbnail_by_blog($blog_id=NULL,$post_id=NULL) {
+		if( empty( $blog_id ) ) {
+			global $current_blog;
+			$blog_id = $current_blog;
+		}
+		if( empty( $post_id ) ) {
+			global $post;
+			$post_id = $post->ID;
+		}
+
+		global $wpdb;
+		$oldblog = $wpdb->set_blog_id( $blog_id );
+
+		$thumbid = has_post_thumbnail( $post_id );
+		$wpdb->set_blog_id( $oldblog );
+		return ($thumbid !== false) ? true : false;
+	}
+
+	function the_post_thumbnail_by_blog($blog_id=NULL,$post_id=NULL,$size='post-thumbnail',$attrs=NULL) {
+		echo get_the_post_thumbnail_by_blog($blog_id,$post_id,$size,$attrs);
+	}
+}
