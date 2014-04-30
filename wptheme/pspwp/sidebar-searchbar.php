7a1807e35a76a5c449904933cf4ec22d9159dfe7
Tim Spinks <tim@ninthlink.com>
2014-04-30 17:34:47 +0000

    added style for drop-down

--- wptheme/pspwp/sidebar-searchbar.php
+++ wptheme/pspwp/sidebar-searchbar.php
@@ -2,6 +2,12 @@
 
 
 ?>
+<style>
+.ac-renderer .ac-row {
+	display: block !important;
+	float: none !important;
+}
+</style>
 <div class="sbr idx s-coll clearfix top-search">
 	<script id="divLocald5606_" type="text/javascript" src="http://widgets.diversesolutions.com/Scripts/Dynamic/widgetTLM.js?curDivID=divLocald5606_&curWidth=470&curHeight=200&type=quicksearch&targetDomain=FaFSw2D+etsEohaGyd9djQ==&aid=85189&ssid=11&productType=1&location=1&fields=013&widgetType=2&muteStyles=true&currentURL=http://www.psplatinum.com/search/"></script>
 </div>
