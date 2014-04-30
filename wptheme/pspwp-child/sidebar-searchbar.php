90a6bd973fbaae7549b1b87687b0521f97d9c3f5
Tim Spinks <tim@ninthlink.com>
2014-04-30 17:35:16 +0000

    added style for search bar

--- wptheme/pspwp-child/sidebar-searchbar.php
+++ wptheme/pspwp-child/sidebar-searchbar.php
@@ -2,7 +2,18 @@
 
 $psp_agent_options = get_option('psp_agent_options');
 
-if ($psp_agent_options['idx']['top_search_js']) {
-	$str = '<div class="sbr idx s-coll clearfix top-search">' . stripcslashes($psp_agent_options['idx']['top_search_js']) . '</div>';
-	print $str;
-} ?>
+$theId = 'top-search-bar-idx-id';
+$targetDomain = get_bloginfo('url') . '/search/';
+
+$aid = $psp_agent_options['idx']['agent_id'];
+
+?>
+<style>
+.ac-renderer .ac-row {
+	display: block !important;
+	float: none !important;
+}
+</style>
+<div class="sbr idx s-coll clearfix top-search">
+	<script id="<?php echo $theId; ?>" type="text/javascript" src="http://widgets.diversesolutions.com/Scripts/Dynamic/widgetTLM.js?curDivID=<?php echo $theId; ?>&amp;curWidth=470&amp;curHeight=200&amp;type=quicksearch&amp;targetDomain=<?php echo $targetDomain; ?>&amp;aid=<?php echo $aid; ?>&amp;ssid=11&amp;productType=1&amp;location=1&amp;fields=013&amp;widgetType=2&amp;muteStyles=false&amp;currentURL=<?php echo get_bloginfo('url'); ?>/search/"></script>
+</div>
