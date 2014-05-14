8bd34c13645b82b25fe607af0a864922fb4a731f
Tim Spinks <tim@ninthlink.com>
2014-05-14 20:58:45 +0000

    js updated

--- wptheme/pspwp/js/custom.js
+++ wptheme/pspwp/js/custom.js
@@ -1,4 +1,4 @@
-(function($) {
+jQuery(function($) {
 
     // show front slider once dom loaded
     $('#tab-slides').css('visibility', 'visible');
@@ -20,14 +20,12 @@
     }(window.location.search.substr(1).split('&')));
 
     // new IDX omnibox fix
-    $('#quicksearch_minprice_list').find('option:first-child').text('No Min');
-    $('#quicksearch_maxprice_list').find('option:first-child').text('No Max');
+    $('#quicksearch_minprice_list').find('option[value="0"]').text('No Min');
+    $('#quicksearch_maxprice_list').find('option[value="0"]').text('No Max');
 
     // Set right side-bar no smaller than left content
     var h = $('.left_main_accnt').height();
     $('.right_bar').css('min-height', h);
-
-
 
 
     // contact form pop-up
@@ -40,64 +38,9 @@
     $('.close-pop-up').click(function(){
         $(this).closest('#contact .pop-up').hide();
         $('#pop-overlay').remove();
-    })
-
-
-
-    // form auto-filler
-    var proptype = $.urlParam['idx-q-PropertyTypes'];
-    var city = $('input#idx-q-Cities.sbr-srch').val();
-    var zip = $.urlParam['idx-q-ZipCodes'];
-    var pricemin = $.urlParam['idx-q-PriceMin'];
-    var pricemax = $.urlParam['idx-q-PriceMax'];
-    var sqft = $.urlParam['idx-q-ImprovedSqFtMin'];
-    var bedsmin = $.urlParam['idx-q-BedsMin'];
-    var bathsmin = $.urlParam['idx-q-BathsMin'];
-
-    $('.dsidx-search-widget-propertyTypes option[value="' + proptype + '"]').attr('selected', 'selected');
-    $('#idx-q-Cities option[value="' + city + '"]').attr('selected', 'selected');
-    $('#idx-q-ZipCodes option[value="' + zip + '"]').attr('selected', 'selected');
-    $('#idx-q-PriceMin.dsidx-price').val(pricemin);
-    $('#idx-q-PriceMax.dsidx-price').val(pricemax);
-    $('#idx-q-ImprovedSqFtMin.dsidx-improvedsqft').val(sqft);
-    $('#idx-q-BedsMin.dsidx-beds').val(bedsmin);
-    $('#idx-q-BathsMin.dsidx-baths').val(bathsmin);
-
-    // advanced search format fix
-    var url = window.location.pathname;
-    url = url.split("/");
-    var part = url[3];
-    if ( part == 'advanced' ) {
-        $('div.left_main_accnt').css("width", "918px");
-    }
-
-
-}(jQuery));
-
-/*
-jQuery(document).ready(function() {
-    jQuery(".showvideopopup").fancybox({
-        fitToView   : true,
-        autoSize    : true,
-        closeClick  : false,
-        openEffect  : 'none',
-        closeEffect : 'none'
     });
-    
-    jQuery("a.fancybox").fancybox(
-        {
-             nextEffect : 'fade',
-             prevEffect : 'fade',
-             type: 'image',
-             arrows : true,
-             loop : true,
-        }
-    );
 
-});
-*/
 
-jQuery(function() {     
     jQuery('#agentmenuimg, #comenuimg').each(function() {
         var agentog = jQuery(this).attr('src');
         jQuery(this).data('agentog', agentog).bind('reset', function() {
