fb6f7814c8637d8558209ade6f7221d3beb24f91
Tim Spinks <tim@ninthlink.com>
2014-05-15 18:19:20 +0000

    tabs updated with arrrows

--- wptheme/pspwp-child/sidebar-tabbedlisting.php
+++ wptheme/pspwp-child/sidebar-tabbedlisting.php
@@ -63,55 +63,55 @@
 
 
     	<ul class="bxslider">
-    	<?php
-	    $args = array('post_type'=>'property', 'posts_per_page'=>50, 'orderby'=>'menu_order', 'order'=>'asc');
-	    $the_query = new WP_Query($args);
-	    $count = 0;
+	    	<?php
+		    $args = array('post_type'=>'property', 'posts_per_page'=>50, 'orderby'=>'menu_order', 'order'=>'asc');
+		    $the_query = new WP_Query($args);
+		    $count = 0;
 
-		while ( $the_query->have_posts() ) : $the_query->the_post(); 
-			$count += 1;
-		?>
-		<li>
-			<div class="t_property">
-		        <a href="<?php the_permalink(); ?>" ><?php the_post_thumbnail( array(170,115), array('class' => 't_thumb') ); ?></a>
-		        <div class="t_title" style="margin-bottom: 10px;">
-		           <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-		        </div>
-		        <div>
-		        	<span class="t_label">price</span>&nbsp;<span class="t_txt">
-					<?php 
-						if ( get_post_meta(get_the_ID(), 'wpcf-current-price-range', true) != '' ) {
-							$price = explode('-', get_post_meta(get_the_ID(), 'wpcf-current-price-range', true));
-							$newprice = array();
-							foreach ($price as $k => $v) {
-								$newprice[] = '$' . number_format(floatval(preg_replace("/[^0-9.]/", "", $v)));
+			while ( $the_query->have_posts() ) : $the_query->the_post(); 
+				$count += 1;
+			?>
+			<li>
+				<div class="t_property">
+			        <a href="<?php the_permalink(); ?>" ><?php the_post_thumbnail( array(170,115), array('class' => 't_thumb') ); ?></a>
+			        <div class="t_title" style="margin-bottom: 10px;">
+			           <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+			        </div>
+			        <div>
+			        	<span class="t_label">price</span>&nbsp;<span class="t_txt">
+						<?php 
+							if ( get_post_meta(get_the_ID(), 'wpcf-current-price-range', true) != '' ) {
+								$price = explode('-', get_post_meta(get_the_ID(), 'wpcf-current-price-range', true));
+								$newprice = array();
+								foreach ($price as $k => $v) {
+									$newprice[] = '$' . number_format(floatval(preg_replace("/[^0-9.]/", "", $v)));
+								}
+								$pricestr = implode(' - ', $newprice);
+								echo $pricestr;
 							}
-							$pricestr = implode(' - ', $newprice);
-							echo $pricestr;
-						}
-						elseif ( get_post_meta(get_the_ID(), 'wpcf-current-price', true) != '' ) {
-							echo '$' . number_format(floatval(preg_replace("/[^0-9]/", "", get_post_meta(get_the_ID(), 'wpcf-current-price', true))));
-						}
-						else {
-							echo 'N/A';
-						}
-					?>
-	        		</span>&nbsp;&nbsp;|&nbsp;&nbsp;<span class="t_label">sq/ft</span>&nbsp;<span class="t_txt"><?php echo types_render_field("square-feet", array()); ?></span>
-		        	<br>
-		        	<span class="t_label">bed/bath</span>&nbsp;<span class="t_txt"><?php echo types_render_field("bed", array()); ?> bed | <?php echo types_render_field("bath", array()); ?> bath</span>
-		        	<br>
-		        </div>
-		        <div id="inline<?php echo $count; ?>" style="text-align: center; display:none;"><?php echo(types_render_field( "video-code", array())); ?><h3><i><?php the_title(); ?></i></h3></div>
-				<a class="showvideopopup t_link" href="#inline<?php echo $count; ?>"><img src="<?php bloginfo('template_url'); ?>/images/t_video_link.png" width="92" height="20"></a>
-				<a target="_blank" class="t_link" href="http://maps.google.com/?q=<?php echo urlencode(get_post_meta(get_the_ID(), 'wpcf-address', true)); ?>"><img src="<?php bloginfo('template_url'); ?>/images/t_map_link.png" width="92" height="20"></a>
-		    </div>
-		</li>
-		
-		<!--
-		<div class="t_spacer">
-	        <img src="<?php bloginfo('template_url'); ?>/images/tabs_large_divider.png" width="15" height="142">
-	    </div> -->
-		<?php endwhile; wp_reset_postdata(); ?>
+							elseif ( get_post_meta(get_the_ID(), 'wpcf-current-price', true) != '' ) {
+								echo '$' . number_format(floatval(preg_replace("/[^0-9]/", "", get_post_meta(get_the_ID(), 'wpcf-current-price', true))));
+							}
+							else {
+								echo 'N/A';
+							}
+						?>
+		        		</span>&nbsp;&nbsp;|&nbsp;&nbsp;<span class="t_label">sq/ft</span>&nbsp;<span class="t_txt"><?php echo types_render_field("square-feet", array()); ?></span>
+			        	<br>
+			        	<span class="t_label">bed/bath</span>&nbsp;<span class="t_txt"><?php echo types_render_field("bed", array()); ?> bed | <?php echo types_render_field("bath", array()); ?> bath</span>
+			        	<br>
+			        </div>
+			        <div id="inline<?php echo $count; ?>" style="text-align: center; display:none;"><?php echo(types_render_field( "video-code", array())); ?><h3><i><?php the_title(); ?></i></h3></div>
+					<a class="showvideopopup t_link" href="#inline<?php echo $count; ?>"><img src="<?php bloginfo('template_url'); ?>/images/t_video_link.png" width="92" height="20"></a>
+					<a target="_blank" class="t_link" href="http://maps.google.com/?q=<?php echo urlencode(get_post_meta(get_the_ID(), 'wpcf-address', true)); ?>"><img src="<?php bloginfo('template_url'); ?>/images/t_map_link.png" width="92" height="20"></a>
+			    </div>
+			</li>
+			
+			<!--
+			<div class="t_spacer">
+		        <img src="<?php bloginfo('template_url'); ?>/images/tabs_large_divider.png" width="15" height="142">
+		    </div> -->
+			<?php endwhile; wp_reset_postdata(); ?>
 		</ul>
 
 
@@ -130,7 +130,9 @@
 
 
     <div class="slide-container">
-		<?php dynamic_sidebar('recently-reduced'); ?> 
+    	<a href="javascript:void(0)" class="dsidx-slideshow-control dsidx-move-up" data-observed="true" style="color: rgb(153, 153, 153);">previous</a>
+		<?php dynamic_sidebar('recently-reduced'); ?>
+		<a href="javascript:void(0)" class="dsidx-slideshow-control dsidx-move-down" data-observed="true" style="color: rgb(0, 0, 0);">next</a>
     </div> <!-- .slide-container -->
 
 
