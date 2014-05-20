2e236d12cf7b38683e5a1499ff28d0b9551a6521
Tim Spinks <tim@ninthlink.com>
2014-05-20 19:33:07 +0000

    more vers updates

--- wptheme/pspwp/functions.php
+++ wptheme/pspwp/functions.php
@@ -214,8 +214,8 @@ function pspwp_scripts() {
 
 
 	// Styles
-	wp_enqueue_style( 'pspwp-style', get_bloginfo( 'stylesheet_url' ) . '?' . filemtime( get_stylesheet_directory() . '/style.css' ), array() );
-	wp_enqueue_style( 'all_css', get_template_directory_uri() . '/css/all_css.css' . '?' . filemtime( get_stylesheet_directory() . '/css/all_css.css'), array() );
+	wp_enqueue_style( 'pspwp-style', get_bloginfo( 'stylesheet_url' ) . '?t=' . filemtime( get_stylesheet_directory() . '/style.css' ), array() );
+	wp_enqueue_style( 'all_css', get_template_directory_uri() . '/css/all_css.css' . '?t=' . filemtime( get_template_directory() . '/css/all_css.css'), array() );
 	wp_enqueue_style( 'jquery-ui-1.8.19.custom', get_template_directory_uri() . '/css/jquery_custom_light/jquery-ui-1.8.19.custom.css', array('pspwp-style') );
 	wp_enqueue_style( 'jquery.ui.tabs', get_template_directory_uri() . '/css/jquery_custom_light/jquery.ui.tabs.css', array('pspwp-style'), $vers );
 	//wp_enqueue_style( 'bxslider.css', get_template_directory_uri() . '/bxslider/jquery.bxslider.css', array('pspwp-style'), $vers );
